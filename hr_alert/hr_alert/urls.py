cbc6af4e6ca36d5c99930c322df8b1a9ea2fc2ce
Abdur Razzaq Qureshi <64024913+qabdurrazzaq@users.noreply.github.com>
2024-08-29 07:48:15 +0000

    jwt auth done

--- hr_alert/hr_alert/urls.py
+++ hr_alert/hr_alert/urls.py
@@ -1,25 +1,15 @@
-"""
-URL configuration for hr_alert project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from admin_auth.views import CreateUserView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/user/register/", CreateUserView.as_view(), name="register"),
+    path("auth/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
+    path("admin_auth/", include("rest_framework.urls")),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
